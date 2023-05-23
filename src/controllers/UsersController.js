111232e41293004b1e26f82332f5e743f380dd32
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-05-23 15:59:27 -0300

    ft update method at UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 
 class UsersController 
 {
@@ -28,7 +28,51 @@ class UsersController
         return response.status(201).json();
     }
 
-    
+    async update(request, response)
+    {
+        const { name, email, password, old_password } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user)
+            throw new AppError("User not found!");
+        
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id)
+            throw new AppError("This email is already in use!");
+
+        if(password && !old_password)
+            throw new AppError("The old password must be informed!");
+        
+        if(password && old_password)
+        {
+            const checkOldPassword = await compare(old_password, user.password);
+
+            if(!checkOldPassword)
+                throw new AppError("The old password doesn`t match!");
+
+            user.password = await hash(password, 8);
+        }
+
+        user.name = name ?? user.name;
+        user.email = email ?? user.email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            password = ?,
+            updated_at = DATETIME('now')
+            WHERE id = ?`,
+            [user.name, user.email, user.password, id]        
+        );
+
+        return response.json();
+    }
 }
 
 module.exports = UsersController;
