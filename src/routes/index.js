3ea2fb8f1a1219f77c4831fe4d58fb84509ebfb1
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-05-24 16:18:53 -0300

    ft movie tags route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const usersRouter = require("./user.routes");
 const movieNotesRouter = require("./movieNotes.routes");
+const movieTagsRouter = require("./movieTags.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/movie_notes", movieNotesRouter);
+routes.use("/movie_tags", movieTagsRouter);
 
 module.exports = routes;
