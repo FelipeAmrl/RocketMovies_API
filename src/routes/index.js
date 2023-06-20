15df203e10f2d6f196d1f9a6ecec0c5e7a1ef48f
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-06-20 15:49:26 -0300

    ft: created sessions routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const usersRouter = require("./user.routes");
 const movieNotesRouter = require("./movieNotes.routes");
 const movieTagsRouter = require("./movieTags.routes");
+const sessionsRouter = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
 routes.use("/movie_notes", movieNotesRouter);
 routes.use("/movie_tags", movieTagsRouter);
+routes.use("/sessions", sessionsRouter);
 
 module.exports = routes;
