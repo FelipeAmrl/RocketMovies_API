e8c265148001ffb503c5ccdfaaa1af3e6ca48b10
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-06-20 21:45:01 -0300

    ft: saving avatar image on database

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,21 @@
+const multer = require('multer');
 const { Router } = require("express");
+const uploadConfig = require('../config/upload');
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 
 const usersRoutes = Router();
 
+const upload = multer(uploadConfig.MULTER);
+
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
 module.exports = usersRoutes;
--- src/server.js
+++ src/server.js
@@ -3,12 +3,15 @@ require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
+const uploadConfig = require("./config/upload");
 const migrationsRun = require("./database/sqlite/migrations");
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 migrationsRun();
 
