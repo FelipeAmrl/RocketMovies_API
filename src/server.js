88ad22576c93fb9b9fbfc3b55664de040ba58d5e
FelipeAmrl <felipeamaral.bio@gmail.com>
2023-06-27 11:40:51 -0300

    ft: add environment variables with dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const cors = require("cors");
 const routes = require("./routes");
@@ -34,7 +35,7 @@ app.use((error, request, response, next) => {
     });
 });
 
-const PORT = 3334;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
     console.log(`Server is running on Port: ${PORT}`);
 });
